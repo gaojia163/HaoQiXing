8c46346c1a4aa412990be6938bb4e410547406a9
gaojia163 <ghgaojia@163.com>
2014-04-01 11:16:15 +0800

    jetty

--- test/src/main/java/com/fyyxz/proxy/Client.java
+++ test/src/main/java/com/fyyxz/proxy/Client.java
@@ -1,9 +1,13 @@
 package com.fyyxz.proxy;
 
+import org.apache.log4j.Logger;
+
 public class Client {
+	static Logger logger = Logger.getLogger(Client.class);
 	public static void main(String[] args) {
 	   PassengerProxy proxy = new PassengerProxy();
 	   Passenger passenger = (Passenger) proxy.obj(new RealPassenger());
+	   logger.info("Client运行");
 	   passenger.buyTicket();
 	}
 }
